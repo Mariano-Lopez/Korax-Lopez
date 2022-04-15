39f0bcdbe0a8cee1b9076e5643f580623f014412
Mariano Lopez <marianoalopez99@gmail.com>
2022-04-15 16:31:34 -0300

    Desafio complementario ciclos

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 // Desafio complementario
 
-//Pedir 2 numeros, sumarlos entre si y mostrar el resultado en consola
+//Pedir 2 numeros, sumarlos entre si, luego pedir otro numero mas para sumarselo al resultado anterior (corta con 0).
 
 //Se le pide al usuario que cargue 2 numeros
-let num1 = parseInt(prompt("Ingrese un número")) 
+/*let num1 = parseInt(prompt("Ingrese un número")) 
 let num2 = parseInt(prompt("Ingrese otro"))
 
 //Se los suma entre si
 let suma = num1 + num2
 
+//Se muestra el resultado
+console.log(num1 + " + " + num2 + " = " + suma)
+
 //"Mientras" que num1 no valga 0 el ciclo sigue
-
 while (num1 !==0){
-    console.log(num1 + " + " + num2 + " = " + suma)
+    //Pido nuevamente un numero para sumarselo al resultado anterior
     num1 = parseInt(prompt("Ingrese nuevamente un numero"))
-    num2 = parseInt(prompt("Ingrese otro"))
-    suma = num1 + num2
-}
+    //Suma
+    sumaSeguida = suma + num1
+    console.log(suma + " + " + num1 + " = " + sumaSeguida)
+    //Suma va a valer sumaSeguida para que el valor del resultado anterior no se pierda
+    suma = sumaSeguida
 
-//Le pido al usuario que ingrese sus 3 animales favoritos
+}*/
+
+//Le pido al usuario que ingrese sus 3 animales favoritos y solo 3 para que el ciclo corte.
+
+
 /*let animal
 
-for(let i = 1; i <= 3; i++){
+console.log("Sus animales favoritos son:")
+
+for(let i = 1; i < 4; i++){
+    
     let animal = prompt("Ingrese su animal favorito")
     console.log("Animal " + i + " = " + animal)
 }*/
